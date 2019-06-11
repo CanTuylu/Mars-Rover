b4575192a31e6c76044523526f5b36a72e6288e4
CanTuylu <51237495+CanTuylu@users.noreply.github.com>
2019-06-11 16:50:42 +0300

    Add files via upload

---
+++ Mars-Rover-Controller-master/NavigationLib/Navigation.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoverMicrosoftVers
+{
+    public class Navigation
+    {
+
+        public string yon { get; set; }
+        public string yol { get; set; }
+        public string yeniYon { get; set; }
+        public int xmap { get; set; }
+        public int ymap { get; set; }
+        public int xstart { get; set; }
+        public int ystart { get; set; }
+        public int xfinal { get; set; }
+        public int yfinal { get; set; }
+
+        public string TurnLeft()
+        {
+            if (yon == "K")
+            {
+                yeniYon = "B";
+            }
+            else if (yon == "G")
+            {
+                yeniYon = "D";
+            }
+            else if (yon == "D")
+            {
+                yeniYon = "K";
+            }
+            else if (yon == "B")
+            {
+                yeniYon = "G";
+            }
+
+            yon = yeniYon;
+            return yon;
+        }
+        public string TurnRight()
+        {
+            if (yon == "K")
+            {
+                yeniYon = "D";
+            }
+            else if (yon == "G")
+            {
+                yeniYon = "B";
+            }
+            else if (yon == "D")
+            {
+                yeniYon = "G";
+            }
+            else if (yon == "B")
+            {
+                yeniYon = "K";
+            }
+            yon = yeniYon;
+            return yon;
+        }
+
+        public int  MoveX(int xstart)
+        {
+            if (yon == "B" || yeniYon == "B" || yon == "b" || yeniYon == "b")
+            {
+                xstart--;
+            }
+            else if (yon == "D" || yeniYon == "D"|| yon == "d" || yeniYon == "d")
+            {
+                xstart++;
+            }
+            xfinal = xstart;
+            if (xfinal>xmap || xfinal<0)
+            {
+                Console.WriteLine("Araç harita dışına çıktı!");
+            }
+            return xstart;
+        }
+        public int MoveY(int ystart)
+        {
+            if (yon == "K" || yeniYon == "K"|| yon == "k" || yeniYon == "k")
+            {
+                ystart++;
+            }
+            else if (yon == "G" || yeniYon == "G"|| yon == "g" || yeniYon == "g")
+            {
+                ystart--;
+            }
+            yfinal = ystart;
+            if (yfinal>ymap || yfinal<0)
+            {
+                
+            }
+            return ystart;
+        }
+
+        
+    }
+}
---
+++ RoverMicrosoftVers/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoverMicrosoftVers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Navigation n = new Navigation();
+
+            string yeniYon = "";
+            Console.WriteLine("Harita boyutunu giriniz:");
+            n.xmap = Convert.ToInt32(Console.ReadLine());
+            n.ymap = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Aracın ilk konumunu giriniz ");
+            Console.WriteLine("X:?");
+            n.xstart = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Y:?");
+            n.ystart = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Yön:?(K, G, D, B)");
+            n.yon = Console.ReadLine();
+
+            Console.WriteLine("Aracın rotasını giriniz:");
+            n.yol = Console.ReadLine();
+
+            for (int i = 0; i < n.yol.Length; i++)
+            {
+                if (n.yol[i] == 'L' || n.yol[i] == 'l')
+                {
+                    n.TurnLeft();
+                    n.xfinal = n.xstart;
+                    n.yfinal = n.ystart;
+
+                }
+                else if (n.yol[i] == 'R' || n.yol[i] == 'r')
+                {
+                    n.TurnRight();
+                    n.xfinal = n.xstart;
+                    n.yfinal = n.ystart;
+                }
+                else if (n.yol[i]=='M' || n.yol[i] == 'm')
+                {
+                    n.MoveX(n.xstart);
+                    n.MoveY(n.ystart);
+                }
+                n.xstart = n.xfinal;
+                n.ystart = n.yfinal;
+            }
+            if (n.xfinal > n.xmap || n.xfinal < 0 || n.yfinal > n.ymap || n.yfinal < 0)
+            {
+                Console.WriteLine("Araç haritadan dışarı çıktı!");
+            }
+            else
+            Console.WriteLine("Araç " + n.xfinal + " " + n.yfinal + " koordinatlarında ve " + n.yon + " yönündedir.");
+        }
+    }
+}
